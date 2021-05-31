981cab28ab9fdab18344b85a8298728d590734e4
David Zidon <david.zidon2@gmail.com>
2021-05-31 21:55:31 +0200

    přidáno zobrazování ikonek pro zamknutá a připnutá témata

--- index.php
+++ index.php
@@ -101,7 +101,7 @@ if($sectionsQuery->rowCount() > 0) {
                                 </div>
             
                                 <div class="section-post-links">
-                                    <a href="odkazNaPost" class="section-post-name">'.htmlspecialchars($post['thread_name']).'</a>
+                                    <a href="odkazNaPost" class="section-post-name">'.( $post['thread_pinned'] ? '<i class="fas fa-thumbtack section-post-pin"></i>':'').( $post['thread_locked'] ? '<i class="fas fa-lock section-post-lock"></i>':'').htmlspecialchars($post['thread_name']).'</a>
                                     <span class="section-post-info">
                                         Založil <a class="section-post-user" href="profile.php?user='.htmlspecialchars($creatorName).'">'.htmlspecialchars($creatorName).'</a> '.htmlspecialchars($activityDate).'
                                     </span>
@@ -141,7 +141,7 @@ if($sectionsQuery->rowCount() > 0) {
                                 </div>
             
                                 <div class="section-post-links">
-                                    <a href="odkazNaPost" class="section-post-name">'.htmlspecialchars($post['thread_name']).'</a>
+                                    <a href="odkazNaPost" class="section-post-name">'.( $post['thread_pinned'] ? '<i class="fas fa-thumbtack section-post-pin"></i>':'').( $post['thread_locked'] ? '<i class="fas fa-lock section-post-lock"></i>':'').htmlspecialchars($post['thread_name']).'</a>
                                     <span class="section-post-info">
                                         Odpověděl <a class="section-post-user" href="profile.php?user='.htmlspecialchars($replyUserName).'">'.htmlspecialchars($replyUserName).'</a> '.htmlspecialchars($activityDate).'
                                     </span>
